9c248396886c3d1961815fbdbcb63dbed2ac41fc
Yasir Mehmood <sheikhusman545@gmail.com>
2024-01-18 19:08:47 +0500

    nothing

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/codeignter/index.php';
+$config['base_url'] = 'http://localhost/codeiginter/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/codeignter/index.php';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
